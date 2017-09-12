5520ef47070b473011759fb28111c8b9a0ef57da
romaxa <romaxa83@ukr.net>
2017-09-12 23:14:58 +0300

    added tag

---
+++ views/partials/footer.php
@@ -0,0 +1,38 @@
+<footer id="footer">
+    <div class="container-fluid">
+        <div class="footer-content row">
+            <div class="col-md-5 col-md-offset-1">
+                <h3 class="center"> <Блог-фантаста></h3>
+                <div class="footer-social">
+                    <span>Мы в социальных сетях</span>
+                    <button class="btn-link"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></button>
+                    <button><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></button>
+                    <button><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></button>
+                    <button><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></button>
+                </div>
+
+
+                <span>О нас</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Animi aperiam asperiores consequatur corporis cumque
+                    delectus dolores expedita incidunt ipsam itaque labore
+                    neque nihil nobis nulla odio, quis quod similique suscipit
+                    ullam voluptas! A amet, animi doloribus error modi repellat vitae.</p>
+            </div>
+
+            <div class="footer-nav col-md-4 col-md-offset-2">
+                <span class="footer-cat">Категории</span>
+                <ul>
+                    <li><a href="#" class="underline">Категория</a></li>
+                    <?php var_dump($cats);?>
+                </ul>
+            </div>
+
+        </div>
+        <div class="copyright row">
+            <p class="copy"> © 2017 Created by Rodomanov Roman</p>
+
+        </div>
+    </div>
+
+</footer>
